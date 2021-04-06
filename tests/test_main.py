d021d97e372d37f27a9d2242ac2114c35f948359
Tharindu N <40334396+truethari@users.noreply.github.com>
2021-04-07 00:06:28 +0530

    Updated test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,16 @@
 import os
+import ntpath
 
 from infomedia  import mediainfo, Worker
 
 video_path = "/home/sample-mp4-file.mp4"
+output_path = "/home/runner/work/infomedia/infomedia"
 
 def test_mediainfo():
     test_mediainfo_value = mediainfo(video_path)
     assert "duration" in test_mediainfo_value["format"]
 
 def test_Worker():
-    worker = Worker(video_path, request_data='False', output_format='json', save_path='/home/runner/work/infomedia/infomedia')
+    worker = Worker(video_path, request_data='False', output_format='json', save_path=output_path)
     worker._application()
-    assert os.path.isfile(os.path.join(video_path[:-4] + ".json"))
+    assert os.path.isfile(os.path.join(output_path, ntpath.basename(video_path[:-4]) + ".json"))
