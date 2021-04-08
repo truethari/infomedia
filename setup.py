3798e9821f8f9f2fa60fef160a7668ebbcba805e
Tharindu N <40334396+truethari@users.noreply.github.com>
2021-04-08 18:16:58 +0530

    Added MIT License

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-import  pathlib
+import pathlib
 
-from    setuptools      import setup, find_packages
+from setuptools     import setup, find_packages
+from infomedia      import __version__
 
-exec(open("infomedia/version.py").read())
-
-here = pathlib.Path(__file__).parent
-with open('README.rst') as readme_file:
-    long_description = readme_file.read()
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="infomedia",
     version=__version__,
     description="This is a Python application that can be used to retrieve media file information such as duration, frame rate, bit rate, etc..",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="tharindu.dev",
     author_email="tharindu.nm@yahoo.com",
     url="https://github.com/truethari/infomedia",
     keywords="video ffmpeg ffprobe",
+    license='MIT',
     project_urls={
         "Source": 'https://github.com/truethari/infomedia/',
         "Bug Tracker": "https://github.com/truethari/infomedia/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
     ],
     packages=['infomedia'],
     include_package_data=True,
